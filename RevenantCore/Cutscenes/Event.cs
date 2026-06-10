1bd7a22b79e198d3d8100b2a687556413225697b
0xFFD800 <lukedgreene@gmail.com>
2026-06-09 18:30:16 -0600

    Add documentation

--- RevenantCore/Cutscenes/Event.cs
+++ RevenantCore/Cutscenes/Event.cs
@@ -6,13 +6,26 @@ using RevenantCore.Cutscenes.Spec;
 
 namespace RevenantCore.Cutscenes;
 
+/// <summary>
+/// A filter which evaluates to true or false based on what events have and have not been completed.
+/// </summary>
+/// <param name="spec">The YAML-deserializable spec for this filter.</param>
 public class EventFilter(EventFilterSpec spec)
 {
+    /// <summary>
+    /// Evaluates the filter.
+    /// </summary>
+    /// <param name="events">The event collection to evaluate this filter in the context of.</param>
+    /// <returns>Whether or not this filter's criteria are fulfilled.</returns>
     public bool Evaluate(EventCollection events) => !spec.HasNone.Any(events.IsComplete)
         && spec.HasAny.Any(events.IsComplete)
         && spec.HasAll.All(events.IsComplete);
 }
 
+/// <summary>
+/// The universe of all possible events and which of them are complete.
+/// </summary>
+/// <param name="allEvents">A YAML-deserializable spec representing all events.</param>
 public class EventCollection(EventSpec[] allEvents)
 {
     /// <summary>
@@ -27,19 +40,37 @@ public class EventCollection(EventSpec[] allEvents)
     /// </summary>
     private readonly HashSet<string> completed = [];
 
+    /// <summary>
+    /// Completes the event, if it exists and its preconditions allow it.
+    /// </summary>
+    /// <param name="evt">The ID of the event to attempt to complete.</param>
     public void Complete(string evt)
     {
         if (events.TryGetValue(evt, out Event? e) && e.CanComplete(this))
             completed.Add(evt);
     }
 
+    /// <summary>
+    /// Checks whether the event mapped to the given ID has been completed.
+    /// </summary>
+    /// <param name="evt">The ID of the event to check for completion.</param>
+    /// <returns>Whether <paramref name="evt"/> has been completed or not.</returns>
     public bool IsComplete(string evt) => completed.Contains(evt);
 
+    /// <summary>
+    /// Removes the event mapped to the given ID from the set of completed events. 
+    /// If the event has not been completed, this method does nothing.
+    /// </summary>
+    /// <param name="evt"></param>
     public void Undo(string evt)
     {
         completed.Remove(evt);
     }
 
+    /// <summary>
+    /// Represents an event which can be completed in gameplay.
+    /// </summary>
+    /// <param name="spec">The YAML-deserializable spec representing this event.</param>
     private class Event(EventSpec spec)
     {
         private readonly Precondition precondition = spec.PreconditionType switch
@@ -50,31 +81,63 @@ public class EventCollection(EventSpec[] allEvents)
             _ => throw new ArgumentException(string.Format("Unrecognized precondition type {0}", Enum.GetName(spec.PreconditionType)))
         };
 
+        /// <summary>
+        /// Whether this event can be completed, based on its precondition.
+        /// </summary>
+        /// <param name="events">The event context to check this precondition against.</param>
+        /// <returns>Whether this event's precondition allows it to be created.</returns>
         public bool CanComplete(EventCollection events) => precondition.Evaluate(events);
     }
 }
 
+/// <summary>
+/// A wrapper around EventFilter specifying the steps which should be taken 
+/// if an event's precondition filter does not allow for it to be created.
+/// </summary>
+/// <param name="spec">The spec of the event which this precondition checks.</param>
 public abstract class Precondition(EventSpec spec)
 {
+    /// <summary>
+    /// The spec of the event which this precondition checks.
+    /// </summary>
     protected readonly EventSpec spec = spec;
     private readonly EventFilter? filter = spec.Preconditions != null ? new(spec.Preconditions) : null;
 
+    /// <summary>
+    /// Evaluates the precondition.
+    /// </summary>
+    /// <param name="events">The event context in which to evaluate the precondition.</param>
+    /// <returns>Whether the precondition evaluated successfully.</returns>
     public bool Evaluate(EventCollection events) => filter?.Evaluate(events) ?? true || Bypass(events);
 
+    /// <summary>
+    /// Evaluates whether or not the event can still be completed if the filter does not pass.
+    /// </summary>
+    /// <param name="events">The event context to evaluate the bypass logic in.</param>
+    /// <returns>Whether or not the event may still be completed.</returns>
     protected abstract bool Bypass(EventCollection events);
 }
 
+/// <summary>
+/// Crashes the game if the precondition fails.
+/// </summary>
 public class ErrorPrecondition(EventSpec spec) : Precondition(spec)
 {
     protected override bool Bypass(EventCollection events) =>
         throw new InvalidOperationException(string.Format("Cannot complete \"{0}\" as preconditions have not been met.", spec.Name));
 }
 
+/// <summary>
+/// Doesn't complete this event if the precondition fails.
+/// </summary>
 public class IgnorePrecondition(EventSpec spec) : Precondition(spec)
 {
     protected override bool Bypass(EventCollection events) => false;
 }
 
+/// <summary>
+/// Forces the precondition to succeed by fulfilling its criteria automatically.
+/// </summary>
 public class ForcePrecondition(EventSpec spec) : Precondition(spec)
 {
     protected override bool Bypass(EventCollection events)
