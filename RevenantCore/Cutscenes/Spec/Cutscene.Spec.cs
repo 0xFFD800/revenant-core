79eb6fa1362f4eebdf5a99b3f2f26d00c64ff0e9
0xFFD800 <lukedgreene@gmail.com>
2026-06-12 21:02:40 -0600

    Require cutscene specs to make cutscenes

--- RevenantCore/Cutscenes/Spec/Cutscene.Spec.cs
+++ RevenantCore/Cutscenes/Spec/Cutscene.Spec.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Frozen;
 using System.Collections.Generic;
+using RevenantCore.Scenes;
 using RevenantCore.Util;
 using YamlDotNet.Serialization;
 
@@ -9,14 +10,48 @@ namespace RevenantCore.Cutscenes.Spec;
 // TODO: more unit tests
 
 /// <summary>
-/// The base dataspec for cutscenes. All other cutscene specs should inherit from this one
+/// The base dataspec for cutscenes. All other cutscene specs should inherit from this one.
 /// </summary>
-public class CutsceneSpec
+public abstract class CutsceneSpec
 {
     /// <summary>
     /// The spec for the filter which will decide if this cutscene is triggered.
     /// </summary>
     public EventFilterSpec Filter { get; set; } = new();
+
+    /// <summary>
+    /// Converts this spec into the corresponding in-memory cutscene object.
+    /// </summary>
+    /// <param name="universe">The universe in which this cutscene should be created.</param>
+    /// <returns>The in-memory cutscene corresponding to this spec's type.</returns>
+    public abstract Cutscene Create(Universe universe);
+}
+
+/// <summary>
+/// The base spec for block cutscenes, which have other cutscenes as children.
+/// </summary>
+public abstract class BlockSpec : CutsceneSpec
+{
+    /// <summary>
+    /// The specs for all the children of this cutscene.
+    /// </summary>
+    public CutsceneSpec[] Children { get; set; } = [];
+}
+
+/// <summary>
+/// The spec for a sequential block, which will execute its children one by one in order.
+/// </summary>
+public class SequentialBlockSpec : BlockSpec
+{
+    public override Cutscene Create(Universe universe) => new SequentialBlock(universe, this);
+}
+
+/// <summary>
+/// The spec for a concurrent block, which will execute its children all at the same time.
+/// </summary>
+public class ConcurrentBlockSpec : BlockSpec
+{
+    public override Cutscene Create(Universe universe) => new ConcurrentBlock(universe, this);
 }
 
 /// <summary>
