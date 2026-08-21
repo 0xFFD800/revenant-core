dba5cc864706d7b19cc69e70268cb3be91e21b1a
0xFFD800 <lukedgreene@gmail.com>
2026-08-21 16:17:21 -0600

    Aded base Interaction logic

---
+++ RevenantCore/Scenes/Interaction.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using RevenantCore.Cutscenes;
+using RevenantCore.Scenes.Spec;
+using RevenantCore.Util;
+
+namespace RevenantCore.Scenes;
+
+/// <summary>
+/// In-game implementation of an interaction area which triggers immediately when entered by the player.
+/// </summary>
+/// <param name="spec">The YAML-deserializable spec on which to base this zone's parameters.</param>
+public class LoadingZone(InteractionAreaSpec spec) : ITickable
+{
+    private Cutscene[] allCutscenes = [];
+    private int index = 0;
+
+    public BoundingBox Bounds => new(spec.BottomLeft.Data, spec.BottomLeft.Data + spec.Bounds.Data);
+
+    public bool IsDead { get; private set; } = false;
+
+    public void Create(Scene scene, FrameTime time)
+    {
+        allCutscenes = [..spec.Cutscenes.Select(c => c.Create(scene.Universe))];
+        index = 0;
+        IsDead = allCutscenes.Length == 0;
+    }
+
+    public void Glean(Scene scene, FrameTime time) { }
+
+    public void Tick(Scene scene, FrameTime time)
+    {
+        if (allCutscenes.Length > 0 && ShouldTrigger(scene, time))
+        {
+            if (index < allCutscenes.Length)
+                scene.Add(allCutscenes[index++], scene, time);
+            else if (spec.SubsequentBehavior == SubsequentBehavior.Loop)
+                index = 0;
+            else if (spec.SubsequentBehavior == SubsequentBehavior.RepeatLast)
+                scene.Add(allCutscenes.Last(), scene, time);
+            else
+                IsDead = true;
+        }
+    }
+
+    protected virtual bool ShouldTrigger(Scene scene, FrameTime time)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/// <summary>
+/// In-game implementation of an interaction area which requires player input before it triggers.
+/// </summary>
+/// <param name="spec">The YAML-deserializable spec on which to base this zone's parameters.</param>
+public class InteractionZone(InteractionAreaSpec spec) : LoadingZone(spec)
+{
+    protected override bool ShouldTrigger(Scene scene, FrameTime time)
+    {
+        throw new NotImplementedException();
+        //return base.ShouldTrigger(scene, time);
+    }
+}
